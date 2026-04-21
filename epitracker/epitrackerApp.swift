d88e51174e19d26b8b22e16bb26e3054ec320fd4
Utilisateur invité <Guest@iMac-27.local>
2026-04-21 12:10:51 +0200

    first commit

---
+++ epitracker/epitrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  epitrackerApp.swift
+//  epitracker
+//
+//  Created by Utilisateur invité on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct epitrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
